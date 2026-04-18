003087a73e32c9a721f4eb95b24a4b9ee2fb2901
Ahmad Dahlan Hafizh <adh10023@nyu.edu>
2026-04-18 11:09:41 +0400

    Machine Lab - Apr 18 | code changed to noDelay

--- src/content/blog/machine-lab-journal/sketch_apr18a/sketch_apr18a.ino
+++ src/content/blog/machine-lab-journal/sketch_apr18a/sketch_apr18a.ino
@@ -1,12 +1,12 @@
 #include <Servo.h>
 
-//Declare all servos 
-Servo servo1;
-Servo servo2;
-Servo servo3;
-Servo servo4;
+// Declare servos
+Servo servo1, servo2, servo3, servo4;
 
-int pos = 0;    
+int pos = 0;
+int step = 10;            
+unsigned long lastMove = 0; 
+const int interval = 15;  // Interval between timer 
 
 void setup() {
   servo1.attach(3);
@@ -16,19 +16,24 @@ void setup() {
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 10) {
+  unsigned long currentMillis = millis();
+
+  // Check if it's time to move based on our interval
+  if (currentMillis - lastMove >= interval) {
+    lastMove = currentMillis;
+
+    pos += step; 
+
+    // Reverse direction if we hit the limits
+    if (pos >= 180 || pos <= 0) {
+      step = -step; 
+    }
     servo1.write(pos);
     servo2.write(pos);
     servo3.write(pos);
     servo4.write(pos);
-    delay(15); 
   }
 
-  for (pos = 180; pos >= 0; pos -= 10) {
-    servo1.write(pos);
-    servo2.write(pos);
-    servo3.write(pos);
-    servo4.write(pos);
-    delay(15);
-  }
+// CODE CONTENT FOR OTHER STUFF HERE!!!
+
 }

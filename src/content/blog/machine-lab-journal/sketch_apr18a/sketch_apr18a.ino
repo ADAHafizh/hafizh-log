cb6c2087c94f6350925bec89afcd121ab86896e4
Ahmad Dahlan Hafizh <adh10023@nyu.edu>
2026-04-18 12:20:56 +0400

    Machine Lab - code changed to have slower interval

--- src/content/blog/machine-lab-journal/sketch_apr18a/sketch_apr18a.ino
+++ src/content/blog/machine-lab-journal/sketch_apr18a/sketch_apr18a.ino
@@ -1,24 +1,49 @@
 #include <Servo.h>
 
+// This is a test of having all motors start from the same initial position 0, and then wait for 5 seconds, and then move them together synchronously 
+
+// To achieve that, we need to create a simple state machine 
+
+bool isMoving = false; 
+
 // Declare servos
 Servo servo1, servo2, servo3, servo4;
 
 int pos = 0;
 int step = 10;            
 unsigned long lastMove = 0; 
-const int interval = 15;  // Interval between timer 
+const int interval = 30;  // Interval between timer 
+const unsigned long waitTime = 3000; // Interval at start 
+
+
 
 void setup() {
   servo1.attach(3);
   servo2.attach(5);
   servo3.attach(6);
   servo4.attach(9);
+
+  // Set initial position 
+  servo1.write(0);
+  servo2.write(0);
+  servo3.write(0);
+  servo4.write(0);
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  // Check if it's time to move based on our interval
+  // State management
+  // Check if we are still at the initial waiting period
+  if (!isMoving) {
+    if (currentMillis >= waitTime) {
+      isMoving = true;
+    }
+    return;
+  }
+
+
+  // Movement Logic 
   if (currentMillis - lastMove >= interval) {
     lastMove = currentMillis;
 
